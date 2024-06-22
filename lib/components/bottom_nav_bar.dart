fda4aa5d79e3b604e2f5f93dab3ce3ad76714d0c
Menna-Ahmed7 <mennaahmed0701@gmail.com>
2024-06-22 22:38:26 +0300

    edit

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -22,54 +22,42 @@ class _CustomButtomNavBarState extends State<CustomButtomNavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<NavBarCubit, NavBarState>(
-      builder: (context, state) {
-        if (state is NavBarHomeState)
-          _selectedIndex = 0;
-        else if (state is NavBarFavState)
-          _selectedIndex = 1;
-        else if (state is NavBarCartState)
-          _selectedIndex = 2;
-        else if (state is NavBarProfileState) _selectedIndex = 3;
-
-        return BottomNavigationBar(
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-          selectedItemColor: Colors.black,
-          showUnselectedLabels: true,
-          unselectedItemColor: Color.fromARGB(255, 86, 86, 86),
-          selectedFontSize: 16,
-          unselectedFontSize: 16,
-          elevation: 0,
-          // type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(
-                FontAwesomeIcons.home,
-              ),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                FontAwesomeIcons.heart,
-              ),
-              label: "Favorites",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                FontAwesomeIcons.cartShopping,
-              ),
-              label: "My Cart",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                FontAwesomeIcons.user,
-              ),
-              label: "Profile",
-            )
-          ],
-        );
-      },
+    return BottomNavigationBar(
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      selectedItemColor: Colors.black,
+      showUnselectedLabels: true,
+      unselectedItemColor: Color.fromARGB(255, 86, 86, 86),
+      selectedFontSize: 16,
+      unselectedFontSize: 16,
+      elevation: 0,
+      // type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            FontAwesomeIcons.home,
+          ),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            FontAwesomeIcons.heart,
+          ),
+          label: "Favorites",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            FontAwesomeIcons.cartShopping,
+          ),
+          label: "My Cart",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            FontAwesomeIcons.user,
+          ),
+          label: "Profile",
+        )
+      ],
     );
   }
 }
